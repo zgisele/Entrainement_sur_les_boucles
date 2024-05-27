eb0d3dc2ccf2cf38be38b5ba35d4c02f42af833e
zgisele <kafouigisele@gmail.com>
2024-05-27 05:10:29 +0000

    version

--- 02__password.rb
+++ 02__password.rb
@@ -1,15 +1,30 @@
-n=5
-while n % 2 == 1
-    h = n/2
-    puts"voici la pyramide:"
-    0.upto(h) do |i|
-        puts " "*(h-i)+"*"*(2*i+1)   
+
+    password = nil
+    def signup
+        puts "Veuillez définir votre mot de passe :"
+        @password = gets.chomp
+        puts "Mot de passe enregistré avec succès !"
     end
-    (h).downto(0) do |i|
-        puts " "*(h-i)+"*"*(2*i+1)        
-    end      
+
+    def login
+        puts "Veuillez entrer votre mot de passe :"
+        input_password = gets.chomp
+        until input_password == @password
+            puts "Mot de passe incorrect. Veuillez réessayer :"
+            input_password = gets.chomp
+        end
+        puts "Connexion réussie !"
+    end
+
+    def welcome_screen
+        puts "Bienvenue sur l'écran d'accueil top secret de la NSA !"
+        puts "Voici quelques informations confidentielles..."
+        # Afficher des informations confidentielles ici
+    end
+# end
+def perform
+    signup
+    login
+    welcome_screen
 end
-puts"nouvel v"
-puts">"
-m=gets.chomp
-n = m
+perform
--- 03_stairway.rb
+++ 03_stairway.rb
@@ -1,1 +1,64 @@
-gg
+class JeuDeLOie
+
+    attr_accessor :position
+    attr_accessor :chiffre
+
+    def initialize
+        @position = 0
+    end
+
+    def lancer_de
+        #   rand(1..6)
+        #puts"une DE contient les chiffres suivants:1,2,3,4,5 et 6"
+        puts"veiller saisie le chiffre afficher par le DE"
+        print">"
+        @chiffre = gets.chomp.to_i
+    end
+
+    def jouer_tour
+        de = lancer_de
+        case de
+        when 5, 6
+            @position += 1
+            puts "Vous avez fait #{de}. Vous avancez d'une marche. Vous êtes maintenant à la marche #{@position}."
+        when 1
+            @position -= 1 if @position > 0
+            puts "Vous avez fait #{de}. Vous descendez d'une marche. Vous êtes maintenant à la marche #{@position}."
+        when 2, 3, 4
+            puts "Vous avez fait #{de}. Rien ne se passe. Vous restez à la marche #{@position}."
+        end
+    end
+    def jouer_partie
+        tours = 0
+        until @position == 10
+            jouer_tour
+            tours += 1
+        end
+        puts "Félicitations ! Vous avez atteint la 10ème marche en #{tours} tours !"
+        tours
+    end
+end
+
+# Simulation de 100 parties pour calculer le nombre moyen de tours
+def average_finish_time(simulations)
+    total_tours = 0
+    simulations.times do
+        jeu = JeuDeLOie.new
+        total_tours += jeu.jouer_partie
+    end
+    average_tours = total_tours / simulations.to_f
+    puts "Le nombre moyen de tours pour atteindre la 10ème marche est de #{average_tours.round(2)}."
+    average_tours
+end
+
+# Exécuter une partie
+#   jeu = JeuDeLOie.new
+#   jeu.jouer_partie
+
+def perform
+    # Calculer les statistiques sur 100 parties
+    average_finish_time(100)
+
+end
+perform
+
